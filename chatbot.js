058ef2fb393ed513b86147c094b1bec50ccd7077
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-09-08 13:49:20 -0500

    took out secret-variables. js from index and step 11 with validation

--- chatbot.js
+++ chatbot.js
@@ -1,15 +1,17 @@
 onEvent("sendBtn", "click", function() {
     //console.log to make sure button is clicked
     console.log("sendBtn clicked!");
-    
-    //setText to display "Test Message" in output-text
-    setText("output-text", "Test Message");
-    
+
     //getValue from user-input or (direct DOM) to confirm input value is being read correctly
-    
-    var userQuestion = getValue("user-input");
+    let userQuestion = getValue("user-input");
     console.log(userQuestion);
     // confirmed question pops up in console
+
+        if (userQuestion === "") {
+        setText("output-text", "Please enter a message");
+    }
     
+    //setText to display "Test Message" in output-text
+    setText("output-text", "");
 
 });
