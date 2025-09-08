4c1ebbad84149a44e6b917b8d0da0b8a2cd23b22
TyQuincia-Oby <tyquinciaoby@gmail.com>
2025-09-08 12:56:31 -0500

    Level 11 Input Value Reading

--- chatbot.js
+++ chatbot.js
@@ -1,5 +1,15 @@
-onEvent("sendBtn", "click", funtion() {
+onEvent("sendBtn", "click", function() {
+    //console.log to make sure button is clicked
     console.log("sendBtn clicked!");
-    setText("output-text", "Hello, World!");
+    
+    //setText to display "Test Message" in output-text
+    setText("output-text", "Test Message");
+    
+    //getValue from user-input or (direct DOM) to confirm input value is being read correctly
+    
+    var userQuestion = getValue("user-input");
+    console.log(userQuestion);
+    // confirmed question pops up in console
+    
 
 });
